832611faf10df62516a7ef42bc9e176093d9ac06
Gaurav Singh Kabdwal <gskabdwal@gmail.com>
2023-07-22 14:52:11 +0530

    Update

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -18,7 +18,6 @@ class MealItem extends StatelessWidget {
     return meal.complexity.name[0].toUpperCase() +
         meal.complexity.name.substring(1);
   }
-  
 
   String get affordabilityText {
     return meal.affordability.name[0].toUpperCase() +
